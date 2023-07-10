52f6902f0e7d5f80c7296734570e4aba42defa9a
Yonathan <yonathan.getachew35@gmail.com>
2023-07-10 16:04:19 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      people: [
+        {
+          name: "Yonathan",
+          birthday: "October 15",
+          number: "###-###-####",
+        },
+        {
+          name: "Wayne",
+          birthday: "January 24",
+          number: "###-###-####",
+        },
+        {
+          name: "Jazer",
+          birthday: "December 21",
+          number: "###-###-####",
+        },
+        // Add more people as needed
+      ],
+    };
+  }
+
+  render() {
+    const { people } = this.state;
+
+    return (
+      <div className="App">
+        {people.map((person, index) => (
+          <div key={index} className="person-info">
+            <h3>name: {person.name}</h3>
+            <h3>birthday: {person.birthday}</h3>
+            <h3>number: {person.number}</h3>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
+
